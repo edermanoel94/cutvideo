977a96ab5ded171ec53f3d0a0b5372d531473429
edermanoel <edercosta94@live.com>
2025-12-08 23:29:27 -0300

    Adding clip object

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <json-c/json.h>
-#include <json-c/json_object.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -11,8 +10,8 @@ struct clip_t {
 
 struct video_t {
   const char *title;
-  const char *input_video_path;
-  const char *clips;
+  const char *input_file;
+  struct clip_t *clips;
 };
 
 int run_ffmpeg(const char *input, struct clip_t clip) {
@@ -35,9 +34,8 @@ int main(int argc, const char *argv[]) {
     exit(0);
   }
 
-  int i, array_length;
+  int i, root_arr_length, clips_arr_length;
 
-  // TODO: use argv for input file
   const char *filename = argv[1];
   struct json_object *root;
 
@@ -47,26 +45,48 @@ int main(int argc, const char *argv[]) {
     return 1;
   }
 
-  array_length = json_object_array_length(root);
+  root_arr_length = json_object_array_length(root);
 
-  printf("%d\n", array_length);
-
-  for (i = 0; i < array_length; i++) {
+  for (i = 0; i < root_arr_length; i++) {
     struct json_object *item = json_object_array_get_idx(root, i);
 
-    struct json_object *titleObj, *input_video_pathObj, *clipsObj;
+    struct json_object *title_obj, *input_file_obj, *clips_obj;
 
-    json_object_object_get_ex(item, "title", &titleObj);
-    json_object_object_get_ex(item, "inputVideoPath", &input_video_pathObj);
-    json_object_object_get_ex(item, "clips", &clipsObj);
+    json_object_object_get_ex(item, "title", &title_obj);
+    json_object_object_get_ex(item, "inputFile", &input_file_obj);
+    json_object_object_get_ex(item, "clips", &clips_obj);
 
     struct video_t video_t = {
-        .title = json_object_get_string(titleObj),
-        .input_video_path = json_object_get_string(input_video_pathObj),
+        .title = json_object_get_string(title_obj),
+        .input_file = json_object_get_string(input_file_obj),
     };
 
+    json_object_object_get_ex(item, "clips", &clips_obj);
+    clips_arr_length = json_object_array_length(clips_obj);
+
     printf("\t###### -----%s----- ######\n", video_t.title);
-    printf("\t%s\n", video_t.input_video_path);
+    printf("\t%s\n", video_t.input_file);
+
+    for (int j = 0; j < clips_arr_length; j++) {
+
+      struct json_object *clip_item = json_object_array_get_idx(clips_obj, j);
+
+      struct json_object *name_obj, *start_time_obj, *end_time_obj;
+
+      json_object_object_get_ex(clip_item, "name", &name_obj);
+      json_object_object_get_ex(clip_item, "startTime", &start_time_obj);
+      json_object_object_get_ex(clip_item, "endTime", &end_time_obj);
+
+      struct clip_t clip_t = {
+          .name = json_object_get_string(name_obj),
+          .start_time = json_object_get_string(start_time_obj),
+          .end_time = json_object_get_string(end_time_obj),
+      };
+
+      printf("\t%s\n", clip_t.name);
+      printf("\t%s\n", clip_t.start_time);
+      printf("\t%s\n", clip_t.end_time);
+    }
   }
 
   // free json object
